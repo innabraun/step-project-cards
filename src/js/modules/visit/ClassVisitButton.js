bc75a6e930ae9a044d557d3beed946ed9737456a
larysakutyrkina <kutyrkina.larysa@gmail.com>
2021-04-28 18:08:03 +0300

    visit modules progress

---
+++ src/js/modules/visit/ClassVisitButton.js
@@ -0,0 +1,7 @@
+export class Button {
+    constructor() {
+    }
+    static createVisitButton(){
+        return `<button id="createVisit" class="btn btn-outline-light" data-target="#createVisitModal" data-toggle="modal">Create Visit</button>`
+    }
+}
